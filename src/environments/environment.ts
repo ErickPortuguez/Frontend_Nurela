bbe890e33f0d1b0298091a113fe1dee9e18a8574
ErickPortuguez <102937335+ErickPortuguez@users.noreply.github.com>
2024-12-11 05:34:11 +0000

    parece que ya esta

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://verbose-space-chainsaw-q55v6vv9774347w6-8080.app.github.dev/'  // URL base de la API
+  apiUrl: 'https://verbose-space-chainsaw-q55v6vv9774347w6-8080.app.github.dev'  // URL base de la API
 };
