2ce47fc3419b483500ddfee12837779926969f7b
ErickPortuguez <102937335+ErickPortuguez@users.noreply.github.com>
2024-12-11 05:32:26 +0000

    parece que ya esta

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8080'  // Cambia la URL para producción
+  apiUrl: 'https://verbose-space-chainsaw-q55v6vv9774347w6-8080.app.github.dev'  // Cambia la URL para producción
 };
